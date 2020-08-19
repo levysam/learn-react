4b76b70c2b047bb2a97547ac8d553b6d790acc28
Levy <levyaraujo42@gmail.com>
2020-08-19 10:46:58 -0400

    CSS loade and Style Loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import react from 'react'
+import React from 'react'
 import { render } from 'react-dom'
+import App from './App'
 
-render(<h2>Hello world</h2>, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
